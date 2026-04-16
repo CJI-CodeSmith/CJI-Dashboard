d0cb586712fdfc911b69e7994a105a1a0d369a8f
Delilah Lopez <Delilah.marinaL@gmail.com>
2026-04-16 17:27:40 -0400

    Created a front end structure and expected components Co-authored-by: Jonathan Cabera <jonathancabera@users.noreply.github.com>

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
-
+import Card from './components/StatCard';
+import ChartPanel from './components/ChartsPanel';
+import CsvDownloadButton from './components/CsvDownloadButton';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 
 const App : React.FC = () => {
 return(
     <>
-    <p> Hello From App</p>
+    <CsvDownloadButton/>
+    <Header/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <ChartPanel/>
+    <ChartPanel/>
+    <ChartPanel/>
+    <Footer/>
     </>
 )
 }
