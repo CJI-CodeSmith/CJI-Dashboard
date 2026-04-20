f41121cd8c7cd7603ccb4506128790fbe72f4d81
Delilah Lopez <Delilah.marinaL@gmail.com>
2026-04-20 15:07:08 -0400

    Created Header file to display Project name and CSV Download button

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import CsvDownloadButton from "./CsvDownloadButton"
 
-export default function Header(){
-    return(
-        <></>
-    )
+//This is where we want to display the  Project name
+//We also need  to house the button to handle CSV Download
+//make sure to include props for csv button
+//add a last refreshed display portion: Optional 
+
+interface HeaderProps {
+    csvData : string;
 }
+
+export default function Header({csvData}): React.FC<HeaderProps>{
+    return(
+        <header>
+        <h1>OSHA Data Center Inspection Dashboard</h1>
+        <p>Cornell Climate Jobs Institute - NAICS 5181210</p>
+        <CsvDownloadButton data={csvData} />
+        </header>
+    );
+};
