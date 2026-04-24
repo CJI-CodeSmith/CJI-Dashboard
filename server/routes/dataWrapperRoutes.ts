7974795b3c38db7a5e34d2b45502a644b06d68de
Katy Wells <katy.wells@verizon.net>
2026-04-24 13:41:37 -0400

    add checkFile function

--- server/routes/dataWrapperRoutes.ts
+++ server/routes/dataWrapperRoutes.ts
@@ -7,6 +7,7 @@ const router = Router();
 
 
 // single route to return all chart info (IDs, embed codes, published dates) for frontend to render as iframes
+//getAllCharts will get chart info from chartInfo.json, NOT an API call.  The getAllCharts function has to check if the json file exists. If not, they get built and data is saved to the json which returns to the front end. If it does exist, just grabs the data from the json and sends to the front end
 router.get('/charts', getAllCharts);
 
 // TODO: add a PUT/charts/:id route when the update functionality is built
